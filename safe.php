aa17bbc67b4af272cf4125a4cf256315ab71e23a
Dan.Tor <danielx0@hotmail.com>
2017-09-24 07:16:43 -0500

    Final Edits (Hopefully)
    This should be the final edit to submit for Rice Hack 2017
    
    Adding a bot to finish things

--- safe.php
+++ safe.php
@@ -1,201 +1,117 @@
 <html><head>
-    <title>Sherlterize - I AM SAFE</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--[if lte IE 8]>
-      <script src="assets/js/ie/html5shiv.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <!--[if lte IE 9]>
-      <link rel="stylesheet" href="assets/css/ie9.css" />
-    <![endif]-->
-    <!--[if lte IE 8]>
-      <link rel="stylesheet" href="assets/css/ie8.css" />
-    <![endif]-->
-  </head><body>
-    <!-- Wrapper -->
-    <div id="wrapper">
-      <!-- Header -->
-      <header id="header">
-        <div class="inner">
-          <!-- Logo -->
-          <a href="index.html" class="logo">
+        <title>Sherlterize - I AM SAFE</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--[if lte IE 8]>
+            <script src="assets/js/ie/html5shiv.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" href="assets/css/main.css">
+        <!--[if lte IE 9]>
+            <link rel="stylesheet" href="assets/css/ie9.css" />
+        <![endif]-->
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="assets/css/ie8.css" />
+        <![endif]-->
+    </head><body>
+        <!-- Wrapper -->
+        <div id="wrapper">
+            <!-- Header -->
+            <header id="header">
+                <div class="inner">
+                    <!-- Logo -->
+                    <a href="index.html" class="logo">
 
 									<span class="symbol"><img src="images/fullhouse.svg" alt=""></span><span class="title">Shelterize</span>
 
 								</a>
-          <!-- Nav -->
-          <nav>
-            <ul>
-              <li>
-                <a href="#menu">Menu</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <!-- Menu -->
-      <nav id="menu">
-        <h2>Menu</h2>
-        <ul>
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a href="helpme.php">Help Me</a>
-          </li>
-          <li>
-            <a href="offerhelp.php">I Want To Help</a>
-          </li>
-          <li>
-            <a href="safe.php">I Am Safe</a>
-          </li>
-          <li>
-            <a href="about.html">About Us</a>
-          </li>
-        </ul>
-      </nav>
-      <!-- Main -->
-
-
-
-
-
-           <div id="main">
-      <div class="inner">
-        <h1>I am Safe&nbsp;</h1> <span class="image main"><img src="images/pic13.jpg" alt=""></span>
-
-        <form class="form-horizontal" role="form" method="POST" action="uploadsafe.php">
-          <div class="form-group"> <label>Name:</label>
-
-            <input type="text" name="namein" class="form-control form-control-lg" placeholder="input name" id="subject" required="" data-validation-required-message="Please enter a subject"> </div>
-
-
-
-
-          <div class="form-group"> <label>Location:</label>
-            <input type="text" name="locationin" class="form-control form-control-lg" placeholder="input location" id="subject" required="" data-validation-required-message="Please enter a location"> </div>
-
-          <div class="form-group"> <label>Phone #:</label>
-            <input type="text" name="phonein" class="form-control form-control-lg" placeholder="input phone#" id="subject" required="" data-validation-required-message="Please enter a subject"> </div>
-
-
- <div class="form-group"> <label>Notes:</label>
-            <input type="text" name="notesin" class="form-control form-control-lg" placeholder="input notes" id="subject" required="" data-validation-required-message="Please enter a subject"> </div>
-
-
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-        
-
-
-       <?php
-
-
-
-//SQL DataBase log in information from the Cpanel in Godaddy
-
-$servername = "localhost";
-$username ="ironmountain";
-$password ="jumpship";
-$dbName ="riceshelter";
-
-
-//create connection
-
-$conn = new mysqli($servername, $username, $password, $dbName);
-
-//check connection
-
-if ($conn -> connect_error){
-	die ("connection failed: " . $conn -> connect_error);
-}
-
-
-$sql = "SELECT * FROM `safe` ";
-
-//If there is a connection display the results 
-//It displays in a table format on the buttom 
-//The echo commands display to the website
-
-$result = $conn ->query($sql);
-
-if ($result-> num_rows >0){
-
- echo '<div class="table-responsive">';
- echo '<table class="table table-striped">';
-
-echo "<thead><tr>
-
-<th>name</th>
-<th>Location</th>
-<th>Phone #</th>
-<th>notes</th>
-<th>date</th>
-
-
-
-</tr>";
-
-echo"</thead>";
-
-	while($row = $result -> fetch_assoc()){
-
-echo"<tbody>
-
-<tr>
-
-<td>" . $row["sname"] . "</td>
-<td>" . $row["sloc"] . "</td>
-<td>" . $row["sphone"] . "</td>
-<td>" . $row["snotes"] ."</td>
-<td>" . $row["sdate"] ."</td>
-
-</tr>";
-
-echo"</tbody>";
-
-		}
-
-        echo'</table>';
-        echo'</div>';
-       
-
-}else{
-	echo"0 results";
-}
-
-$conn->close();
-
-
-?>
-
-
-
-
-
-
-
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
-            rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor
-            fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros
-            ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis
-            non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at,
-            euismod in lectus. Pellentesque habitant morbi tristique senectus et netus
-            et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat
-            maximus. Pellentesque aliquam maximus risus, vel venenatis mauris vehicula
-            hendrerit.</p>
-          <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque
-            venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam,
-            vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur
-            id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis
-            elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
-        </div>
-      </div>
-      <!-- Footer -->
-      <footer id="footer">
+                    <!-- Nav -->
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#menu">Menu</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <!-- Menu -->
+            <nav id="menu">
+                <h2>Menu</h2>
+                <ul>
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="helpme.php">Help Me</a>
+                    </li>
+                    <li>
+                        <a href="offerhelp.php">I Want To Help</a>
+                    </li>
+                    <li>
+                        <a href="safe.php">I Am Safe</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About Us</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- Main -->
+            <div id="main">
+                <div class="inner">
+                    <h1>I am Safe&nbsp;</h1>
+                    <span class="image main"></span>
+                    <form class="form-horizontal" role="form" method="POST" action="uploadsafe.php">
+                        <div class="form-group">
+                            <label>Name:</label>
+                            <input type="text" name="namein" class="form-control form-control-lg" placeholder="input name" id="subject" required="" data-validation-required-message="Please enter a subject">
+                        </div>
+                        <div class="form-group">
+                            <label>Location:</label>
+                            <input type="text" name="locationin" class="form-control form-control-lg" placeholder="input location" id="subject" required="" data-validation-required-message="Please enter a location">
+                        </div>
+                        <div class="form-group">
+                            <label>Phone #:</label>
+                            <input type="text" name="phonein" class="form-control form-control-lg" placeholder="input phone#" id="subject" required="" data-validation-required-message="Please enter a subject">
+                        </div>
+                        <div class="form-group">
+                            <label>Notes:</label>
+                            <input type="text" name="notesin" class="form-control form-control-lg" placeholder="input notes" id="subject" required="" data-validation-required-message="Please enter a subject">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    <!--?php //SQL DataBase log in information from the Cpanel in Godaddy $servername="localhost"
+                    ; $username="ironmountain" ; $password="jumpship" ; $dbName="riceshelter"
+                    ; //create connection $conn=n ew mysqli($servername, $username, $password,
+                    $dbName); //check connection if ($conn --->connect_error){ die ("connection failed: " . $conn -&gt; connect_error);
+                        } $sql = "SELECT * FROM `safe` "; //If there is a connection display the
+                        results //It displays in a table format on the buttom //The echo commands
+                        display to the website $result = $conn -&gt;query($sql); if ($result-&gt; num_rows
+                        &gt;0){ echo '
+                        <div class="table-responsive">'; echo '
+                            '; echo "
+                                "; echo""; while($row = $result -&gt; fetch_assoc()){ echo"
+                                "; echo""; } echo'<table class="table table-striped"><thead>
+                                    <tr>
+                                        <th>name</th>
+                                        <th>Location</th>
+                                        <th>Phone #</th>
+                                        <th>notes</th>
+                                        <th>date</th>
+                                    </tr></thead><tbody>
+                                    <tr>
+                                        <td>" . $row["sname"] . "</td>
+                                        <td>" . $row["sloc"] . "</td>
+                                        <td>" . $row["sphone"] . "</td>
+                                        <td>" . $row["snotes"] ."</td>
+                                        <td>" . $row["sdate"] ."</td>
+                                    </tr></tbody></table>'; echo'</div>'; }else{ echo"0 results";
+                        } $conn-&gt;close(); ?&gt;
+                        
+                        
+                </div>
+            </div>
+            <!-- Footer -->
+            <footer id="footer">
                 <div class="inner">
                     <section>
                         <h1>Shelterize</h1>
@@ -223,15 +139,15 @@ $conn->close();
                     </ul>
                 </div>
             </footer>
-    </div>
-    <!-- Scripts -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/skel.min.js"></script>
-    <script src="assets/js/util.js"></script>
-    <!--[if lte IE 8]>
-      <script src="assets/js/ie/respond.min.js"></script>
-    <![endif]-->
-    <script src="assets/js/main.js"></script>
-  
+        </div>
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/skel.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <!--[if lte IE 8]>
+            <script src="assets/js/ie/respond.min.js"></script>
+        <![endif]-->
+        <script src="assets/js/main.js"></script>
+    
 
 </body></html>
